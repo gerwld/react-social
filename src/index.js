5490b7de77df0e4b33b1aa855038881223e0e46d
Patryk Jaworski <pjaworski.dev@gmail.com>
2021-08-14 12:45:36 +0200

    Put all data into state.js

--- src/index.js
+++ src/index.js
@@ -3,44 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from './redux/state';
 
-
-var postData = [
-  { id: 0, cont: "Hi there!! 2007 is rock!", likes: "23" },
-  { id: 1, cont: "Deez nuts... Today everything is fine, just vibing with my famity #coolday", likes: "28" },
-  { id: 2, likes: "999" }
-];
-
-let dialogsData = [
-  { id: "0", name: "Andrew K" },
-  { id: "1", name: "Anton B" },
-  { id: "2", name: "Richard M" },
-  { id: "3", name: "Sergey K" },
-  { id: "4", name: "Patrick J" },
-  { id: "5", name: "Evgenii K" },
-  { id: "6", name: "Somebody O" },
-  { id: "7", name: "Anastasia I" },
-  { id: "8", name: "Svetlana W" },
-  { id: "9", name: "Andrew K" },
-  { id: "10", name: "Andrew K" }
-];
-
-let messagesData = [
-  { m: "Hi there", userdata: "Anton B", userid: "1" },
-  { m: "Hi", userid: "0" },
-  { m: "How are you?", userdata: "Anton B", userid: "1" },
-  { m: "I'm ok. Do you played in a new Witcher?", userid: "0" },
-  { m: "Oh yeah. It's so cool.", userdata: "Anton B", userid: "1" },
-  { m: "And grapics is just unbeliveble! Cant imagine that CD's do that", userdata: "Anton B", userid: "1" },
-  { m: "Right.", userid: "0" },
-  { m: "I'm playing in it for like ~30 hours without even one break.", userid: "0" },
-  { m: "And i guess its the best game i've ever seen", userid: "0" },
-
-];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App profileData={postData} dialogsData={dialogsData} dialMessData={messagesData}/>
+    <App profileData={state.postData} dialogsData={state.dialogsData} dialMessData={state.messagesData}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
