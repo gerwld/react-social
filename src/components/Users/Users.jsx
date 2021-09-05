bbee510a981ca375f370413082a3eabb15e244b4
Patryk Jaworski <pjaworski.dev@gmail.com>
2021-09-05 12:49:33 +0200

    Add unsubscribe callback

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import s from './Users.module.css';
 
 const Users = (props) => {
-
     return (
         <div>
             <span className={s.title}>Friends</span>
             <div className={s.users_section}>
-                {props.users.map(u => <div key={u.id} className={s.user_block}>
+                {props.users.map(u => <div key={u.id} className={`${s.user_block} ${u.followed ? s : s.user_unsub}`}>
                     <div className={s.user_avatar}><img src={`/images/avatars/avatar-${u.avaHash}.png`}></img></div>
                     <div class={s.user_mainInfo}>
                         <div className={s.user_name}>{u.name}</div>
@@ -15,7 +14,9 @@ const Users = (props) => {
                         <div className={s.action_buttons}><a>Write a message</a> | <a>Call</a></div>
                     </div>
                     <div className={s.user_location}>{u.loc}</div>
-                    <div className={`${s.action_buttons} ${s.action_user_btn}`}><a>Unsubscribe</a><a>Block user</a><a>Add to list</a></div>
+                    <div className={`${s.action_buttons} ${s.action_user_btn}`}>
+                        <a onClick={() => props.unfollowUser(u.id)}>{u.followed ? "Unsubscribe" : "Subscribe"}</a>
+                        <a>Block user</a><a>Add to list</a></div>
                     
                 </div>)}
             </div>
