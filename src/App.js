b40f32532a4f0705d7edaa2edfb9bf75b06b4d20
Patryk Jaworski <pjaworski.dev@gmail.com>
2021-09-13 01:07:50 +0200

    Fix UI scaling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
-import Login from './components/Login/Login';
+import LoginContainer from './components/Login/Login';
 
 
 const App = (props) => {
@@ -24,7 +24,7 @@ const App = (props) => {
         
         <Route path="/users" render = { () => <UsersContainer />} />
         <Route path="/dialogs" render={() => <DialogsContainer />} />
-        <Route path="/login" render = {() => <Login />} />
+        <Route path="/login" render = {() => <LoginContainer />} />
       </div>
     </div>
   );
