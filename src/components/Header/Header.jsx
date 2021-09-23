f1682e5d84d9e25f9c119f5478f55cb703d10801
Patryk Jaworski <pjaworski.dev@gmail.com>
2021-09-23 15:02:22 +0200

    Add login/log out buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = (props) => {
         <header className={s.app_header}>
             <NavLink to="/"><img alt="Logo" src={logo} width="25px" height="auto"></img></NavLink>
             <nav className={`${s.main_nav} main_nav`}>
-              { props.isAuth ? <span>Welcome back, <NavLink to='/profile'>{props.data.login}</NavLink></span> : <NavLink to='/login'>Login</NavLink> }
+              { props.isAuth ? <span><NavLink to='/log-out'>Log Out <i class="fas fa-sign-out-alt" /></NavLink></span> : <NavLink to='/login'>Login <i class="fas fa-sign-in-alt"></i></NavLink> }
             </nav>
         </header>
     );
