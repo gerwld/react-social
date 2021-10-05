924dabe75bed38b43dc806e7ecefc9afcf3e8280
Patryk Jaworski <pjaworski.dev@gmail.com>
2021-10-05 22:10:34 +0200

    Disable proxy for some time

--- src/api/api.js
+++ src/api/api.js
@@ -14,8 +14,10 @@ const instance = axios.create({
     headers: {
         "API-KEY": "781b8b79-ffcd-41da-9a0b-a1374f504632"
     },
-    baseURL: "https://immense-forest-91899.herokuapp.com/https://social-network.samuraijs.com/api/1.0/"
+    baseURL: "https://social-network.samuraijs.com/api/1.0/"
 });
+
+// Proxy for CORS: https://immense-forest-91899.herokuapp.com/
 
 export const usersAPI = {
     getUsers(pageSize = 6, pageNumber = 1, friends, search) {
@@ -80,7 +82,7 @@ export const authAPI = {
 
 export const entertaimentAPI = {
     getPosts(currPage, serving = 5) {
-        return axios.get(`https://immense-forest-91899.herokuapp.com/https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=${serving}&page=${currPage}&apiKey=1ad6438c671a48ad8c6a0731d8c1eabf`).then(r => r.data.articles);
+        return axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&pageSize=${serving}&page=${currPage}&apiKey=1ad6438c671a48ad8c6a0731d8c1eabf`).then(r => r.data.articles);
     }
 }
 
